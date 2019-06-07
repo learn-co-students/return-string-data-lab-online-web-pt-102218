6bafa8e8db9f8ad4b56e1a8fa88b080444df96fc
Matthew Sherman <msherman0801@gmail.com>
2019-06-07 15:13:13 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,40 @@
 class ProductsController < ApplicationController
 
+    def new
+        @product = Product.new
+    end
+
+    def create
+        prod = Product.create(product_params)
+        redirect_to products_path
+    end
+
+    def index
+        @products = Product.all 
+    end
+
+    def inventory
+        prod = Product.find(params[:id])
+        if prod.inventory.to_i > 0
+            render plain: "true"
+        else
+            render plain: "false"
+        end
+    end
+
+    def description
+        prod = Product.find(params[:id])
+        if prod.description
+            render plain: prod.description
+        else
+            render plain: "false"
+        end
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:description, :inventory, :name, :price)
+    end
+
 end
--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -2,6 +2,8 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.integer :inventory
+      t.string :description
       t.integer :price
       t.timestamps null: false
     end
