b8c701b0806a12d48aaa9136c6e42f51a35e1a86
github username <github email address>
2019-06-14 17:20:16 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,15 @@
 class ProductsController < ApplicationController
 
+    def index
+        @products = Product.all
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        binding.pry
+    end
+
 end
