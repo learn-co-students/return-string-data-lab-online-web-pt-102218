6f286eb8e5e03f094c162e37e75bee6a2b8505c9
Arye Eidelman <aryedov.e@gmail.com>
2019-06-14 18:18:28 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,32 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def description
+    get_product
+    render plain: @product.description
+  end
+
+  def inventory
+    get_product
+    # byebug
+    render plain: @product.in_stock?.to_s
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
+
+  def get_product
+    @product = Product.find_by(params[:id])
+  end
 end
